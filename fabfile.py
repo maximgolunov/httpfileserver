6512d789700dd8654fdaa80643cbacc800cfeecb
Maxim Golunov <maxim.golunov@gmail.com>
2013-12-27 19:54:49 +0000

    deployment improvements

--- fabfile.py
+++ fabfile.py
@@ -5,22 +5,23 @@ env.use_ssh_config = True
 env.hosts = ['223.197.230.172']
 #env.hosts = ['mgportfolio', '223.197.230.172']
 
+prog_name = 'http_fs'
+
 def deploy():
     uname = run('uname -s').lower()
-    sudo('mkdir -p /opt/http_fs', warn_only=True)
-    sudo('mkdir /var/log/http_fs', warn_only=True)
-    sudo('chmod -R og+w /var/log/http_fs')
+    sudo('mkdir -p /opt/%s' % (prog_name, ), warn_only=True)
+    sudo('mkdir -p /var/log/%s' % (prog_name, ), warn_only=True)
+    sudo('chmod -R og+w /var/log/%s' % (prog_name, ))
     sudo('easy_install tornado futures')
     
-    put('http_fs/*.py', '/opt/http_fs/', mirror_local_mode = True, use_sudo = True)
-    put('http_fs/*.xml', '/opt/http_fs/', mirror_local_mode = True, use_sudo = True)
+    put('http_fs/*.py', '/opt/%s/' % (prog_name, ), mirror_local_mode = True, use_sudo = True)
     put('etc/*.conf', '/etc/', mirror_local_mode = True, use_sudo = True)
     put('etc/supervisor/conf.d/*.conf', '/etc/supervisor/conf.d/', mirror_local_mode = True, use_sudo = True)
     
     if 'linux' in uname:
-        put('etc/logrotate.d/http_fs', '/etc/logrotate.d/http_fs', mirror_local_mode = True, use_sudo = True)
+        put('etc/logrotate.d/*', '/etc/logrotate.d/', mirror_local_mode = True, use_sudo = True)
     elif 'darwin' in uname:
-        put('etc/newsyslog.d/eu.mgportfolio.http_fs.conf', '/etc/newsyslog.d/eu.mgportfolio.http_fs.conf', mirror_local_mode = True, use_sudo = True)
+        put('etc/newsyslog.d/*', '/etc/newsyslog.d/', mirror_local_mode = True, use_sudo = True)
     
     sudo('supervisorctl update')
-    sudo('supervisorctl restart http_fs')
+    sudo('supervisorctl restart %s' % (prog_name, ))
